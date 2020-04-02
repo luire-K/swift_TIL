e51fc45c76482bd93c851a66f485980ff561de61
eklee <eklee.hello@gmail.com>
2020-04-02 22:57:05 +0900

    200402_rxswift.md file add

---
+++ document/200402_rxswift.md
@@ -0,0 +1,24 @@
+[https://medium.com/flawless-app-stories/getting-started-with-rxswift-and-rxcocoa-5534cf2902b7](https://medium.com/flawless-app-stories/getting-started-with-rxswift-and-rxcocoa-5534cf2902b7)를 따라해가며 공부했다.
+
+오늘은 이 중 **LoginViewController** 부분을 따라하며 공부했다.
+
+사용했었던 코드 중 `disposed(by: disposeBag)`, `.share(replay: 1)`은 실제 동작을 보고 바로 감을 잡았는데 `throttle`의 경우는 와닿지가 않아서 어떤 일을 하는지 찾아보고, 다시 동작을 보고 감을 잡았다.
+
+`throttle`은 이벤트가 발생하면 해당 이벤트를 흘려 보내고 이후 **주어진 시간동안의 다른 이벤트들은 무시하고 해당시간이 흐른 후 무시된 이벤트들 중 가장 최신의 이벤트를 흘려보낸다.**
+
+![throttle](./image/throttle)
+
+작성한 코드를 보면
+`.throttle(throttleInterval, scheduler: MainScheduler.instance)` 인데,
+이 경우에는 textField에 연속된 입력에 일일이 다 반응하지 않고 0.1초 안에 하나의 이벤트만 통과시키게 되는 것.
+
+![combineLatest](./image/combineLatest)
+`.combineLatest`의 경우, 코드만 보고 둘을 합치는 건가 생각하고 있다가 검색 해 보니 두 개의 시퀀스를 하나로 합치는 것이고, 
+
+두 시퀀스는 각각의 최초 이벤트를 발생시켜야 한다는 것도 알 수 있었다.
+
+RxSwift를 공부 해봐야지 하고 찾아볼 때마다 이건 너무 어렵고 복잡하다고 생각했었는데,
+
+ 막상 이렇게 직접 동작하는 것을 보고 자료를 찾아보니 "아 이렇게 동작한다는 거구나!" 하고 이해가 된다.
+
+이렇게 공부하면서 간단한 프로젝트를 해 보면 좋을거 같다. 
--- document/Rxswift/StarterProject/CoffeeShop/CoffeeShop/ViewControllers/LoginViewController.swift
+++ document/Rxswift/StarterProject/CoffeeShop/CoffeeShop/ViewControllers/LoginViewController.swift
@@ -7,20 +7,62 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class LoginViewController: UIViewController {
-  @IBOutlet private weak var emailTextfield: UITextField!
-  @IBOutlet private weak var passwordTextfield: UITextField!
-  @IBOutlet private weak var logInButton: UIButton!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-  
-  @IBAction private func logInButtonPressed() {
-    let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
-    let initialViewController = mainStoryboard.instantiateInitialViewController()!
+    @IBOutlet private weak var emailTextfield: UITextField!
+    @IBOutlet private weak var passwordTextfield: UITextField!
+    @IBOutlet private weak var logInButton: UIButton!
+    // 추가한 코드 4
+    private let throttleInterval = 0.1
+    // 추가한 코드 1
+    private let disposeBag = DisposeBag()
     
-    UIApplication.changeRoot(with: initialViewController)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 추가한 코드 3
+        let emailValid = emailTextfield
+        .rx
+        .text // 1 RxCocoa의 extension 중 하나인 text덕분에 obsevable한 변수로 UItextField의 text parameter에 도달 할 수 있음.
+        .orEmpty // 2 textfield의 데이터는 string 또는 nil로 반환. orEmpty는 데이터가 항상 문자열 타입으로 제공되도록 함.
+            .throttle(throttleInterval, scheduler: MainScheduler.instance)
+            .map { self.validateEmail(with: $0) } // 3 입력 유효성은 validateEmail메소드를 통해 확인. emailValid변수가  Bool type Observable 변수로 바뀌길 원하기 때문에 map함수로 String을 Bool Type으로 입력을 변환.
+        .debug("emailVaild", trimOutput: true) // 4 콘솔에서 이벤트 과정을 볼 수 있음. 이 옵션을 추가할 필요는 없지만 프로세스 관찰을 위해 추가.
+        .share(replay: 1) // 5 만약 이 obsevable변수를 구독하는 다른 관찰자가 있다면 이 프로세스가 반복되지 않게.
+        
+        // 추가한 코드 5
+        let passwordValid =  passwordTextfield.rx.text.orEmpty
+            .throttle(throttleInterval, scheduler: MainScheduler.instance)
+            .map { $0.count >= 6 } // 1 비밀번호 입력에 6자 이상 필요. 요구사항을 map함수에 작성.
+        .debug("passworldValid", trimOutput: true)
+        .share(replay: 1)
+        
+        // 추가한 코드 6
+        let everythingValid = Observable
+            .combineLatest(emailValid, passwordValid) { $0 && $1 } // 1 combineLatest은 Rxswift의 연결연산자 중 하나. 하나의 Observable 변수에 동일한 유형의 Observable 변수를 여러 개 모을 수 있음.
+        .debug("everythingValid", trimOutput: true)
+        .share(replay: 1)
+        
+        // 추가한 코드 7
+        everythingValid
+            .bind(to: logInButton.rx.isEnabled) // 1 bind(to:) 함수를 사용하면 Observable 변수를 동일한 유형으로 연결할 수 있다 . RxCocoa의 extension 중 하나인 버튼의 isEnabled변수를 Observable변수 rx.isEnabled 로 바꿔줌.
+            .disposed(by: disposeBag) // 2 bind(to:) 함수는 일회용 입력 변수를 반환함. LoginViewController 클래스가 dealloc되었을때 만들었던 obsever를 폐기하기 위해 필요하다.
+        
+  }
+    //추가한 코드 2
+    private func validateEmail(with email: String) -> Bool {
+      let emailPattern = "[A-Z0-9a-z._%+-]+@([A-Za-z0-9.-]{2,64})+\\.[A-Za-z]{2,64}"
+      let predicate = NSPredicate(format:"SELF MATCHES %@", emailPattern)
+      
+      return predicate.evaluate(with: email)
+    }
+    
+    @IBAction private func logInButtonPressed() {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let initialViewController = mainStoryboard.instantiateInitialViewController()!
+        
+        UIApplication.changeRoot(with: initialViewController)
   }
 }
